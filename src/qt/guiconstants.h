06120b283a0247fd029ca9c12480f614a50f7b12
Cannabiscoinv2 <andrewmalta@protonmail.com>
2024-10-12 14:38:32 +0200

    Updated link
    Updated website link.

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -49,7 +49,7 @@ static const int MAX_URI_LENGTH = 255;
 #define SPINNER_FRAMES 36
 
 #define QAPP_ORG_NAME "CannabisCoin"
-#define QAPP_ORG_DOMAIN "cannabiscoin.net"
+#define QAPP_ORG_DOMAIN "cannabiscoin.green"
 #define QAPP_APP_NAME_DEFAULT "CannabisCoin-Qt"
 #define QAPP_APP_NAME_TESTNET "CannabisCoin-Qt-testnet"
 
